9489345d58ea316728cd8b11ce325e1cb723c91d
Thiago Mota <thiago.mota@agrosandri.com.br>
2020-04-23 10:52:17 -0300

    Função de compilação refatorada

--- CompilePrograms/CompilePrograms.ps1
+++ CompilePrograms/CompilePrograms.ps1
@@ -1,4 +1,4 @@
-$PRGPATH='C:\Users\thiago.mota\Documents\Projetos\SandriProtheus'
+$PRGPATH='C:\Users\thiago.mota\Documents\Projetos\SandriProtheus\faturamento\ponto entrada'
 $COMPILELIST='C:\Temp\compile.lst'
 $OUTRESULT='C:\Temp\'
 $PROTHEUS='C:\TOTVS\Dev120127\Protheus\bin\appserver'
@@ -6,26 +6,65 @@ $APPSERVER_EXE=$PROTHEUS + '\appserver.exe'
 $ENVCOMPILE='comp_custom'
 $INCLUDES='C:\Users\thiago.mota\Documents\Projetos\SandriProtheus\.Includes'
 
-If (Get-Item $COMPILELIST) {
-    Remove-Item $COMPILELIST
+$SEPARATOR="".PadRight(80,"=")
+$PRG_COMPILAR=$null
+
+function ExecutarCompilacao() {
+
+    LimparArquivosAntigos
+
+    If (ListarArquivos) {
+        compilarProgramas
+        desfragmentarRPO
+        limpaArquivosPreCompilacao
+    }
+    MostraStatus "Compilação de fontes finalizada!"
 }
 
-Write-Host $SEPARATOR
-Write-Host  Obtendo Lista dos programas a compilar
-Write-Host $SEPARATOR
-
-Get-ChildItem  -Path $PRGPATH -File -Recurse -Force -Include *.PRW,*.PRG,*.PRX `
-| Where { $_.FullName -notlike "*\.git\*" `
--and $_.FullName -notlike "*\.Includes\*" `
--and $_.FullName -notlike "*\.vscode\*" `
-} `
-| Format-Table -HideTableHeaders -Property @{e={$_.FullName + ";"}; width = 255} -AutoSize `
-| Out-File -Path $COMPILELIST -NoNewline -Encoding "Windows-1252" -Width 255
-
-If (Get-Item $COMPILELIST) {
+function MostraStatus($texto) {
     Write-Host $SEPARATOR
-    Write-Host  Compilando Programas listados
+    Write-Host  $texto
     Write-Host $SEPARATOR
+}
+
+function LimparArquivosAntigos {
+
+    MostraStatus "Limpando arquivos de Compilação existentes!"
+
+    # Limpar os arquivos de compilação, erros e sucesso
+    Remove-Item $COMPILELIST -ErrorAction SilentlyContinue
+    Remove-Item ($OUTRESULT + "compile_errors.log" ) -ErrorAction SilentlyContinue
+    Remove-Item ($OUTRESULT + "compile_success.log" ) -ErrorAction SilentlyContinue
+}
+
+function ListarArquivos {
+
+    MostraStatus "Obtendo Lista dos programas a compilar..."
+
+    Get-ChildItem  -Path $PRGPATH -File -Recurse -Force -Include *.PRW,*.PRG,*.PRX `
+    | Where { $_.FullName -notlike "*\.git\*" `
+    -and $_.FullName -notlike "*\.Includes\*" `
+    -and $_.FullName -notlike "*\.vscode\*" `
+    } `
+    | Format-Table -HideTableHeaders -Property @{e={$_.FullName + ";"}; width = 255} -AutoSize -OutVariable PRG_COMPILAR
+
+    if ($PRG_COMPILAR.Count -gt 0) {
+        MostraStatus "Encontrados $($PRG_COMPILAR.Count -4) programas para compilar. Gerando arquivo $COMPILELIST ..."
+
+        $PRG_COMPILAR | Out-File -Path $COMPILELIST -NoNewline -Encoding "Windows-1252" -Width 255
+
+        If (Get-Item $COMPILELIST) { return $True } else { return $False}
+
+    } else {
+        MostraStatus "ERRO: Nenhum programa encontrado para compilar"
+        return $False
+    }
+
+}
+
+function compilarProgramas {
+
+    MostraStatus "Compilando Programas listados..."
 
     $AppplyCommand = ('& ' + $APPSERVER_EXE + ' -compile ' `
     + ' -files="'+$COMPILELIST +'"' `
@@ -35,28 +74,35 @@ If (Get-Item $COMPILELIST) {
     + ' -outreport="'+ $OUTRESULT +'"' `
     )
 
+    Write-Host Comando:
     Write-Host $AppplyCommand
+    Write-Host $SEPARATOR
+
     Invoke-Expression $AppplyCommand
+
+    MostraStatus "Fim da Compilação"
 
 }
 
-Write-Host $SEPARATOR
-Write-Host Desfragmentando o RPO
-Write-Host $SEPARATOR
 
-$AppplyCommand = ("& " + $APPSERVER_EXE + " -compile -defragrpo -env="+ $ENVCOMPILE )
-Invoke-Expression $AppplyCommand
+function desfragmentarRPO {
 
+    MostraStatus "Desfragmentando o RPO..."
 
-Write-Host $SEPARATOR
-Write-Host Limpando arquivos de compilação
-Write-Host $SEPARATOR
+    $AppplyCommand = ("& " + $APPSERVER_EXE + " -compile -defragrpo -env="+ $ENVCOMPILE )
+    Invoke-Expression $AppplyCommand
 
-Get-ChildItem  -Path $PRGPATH -File -Recurse -Force `
--Include *.ppo,*.errprw,*.errprx,*.errprg,*.erx_PRW,*.erx_PRX,*.erx_PRG,*.ppx_PRW,*.ppx_PRX,*.ppx_PRG `
-| Remove-Item -Force
+    MostraStatus "Fim da Desfragmentação"
+}
 
-Write-Host $SEPARATOR
-Write-Host Compilação de fontes finalizada!
-Write-Host $SEPARATOR
+function limpaArquivosPreCompilacao {
 
+    MostraStatus "Limpando arquivos de compilação"
+
+    Get-ChildItem  -Path $PRGPATH -File -Recurse -Force `
+    -Include *.ppo,*.errprw,*.errprx,*.errprg,*.erx_PRW,*.erx_PRX,*.erx_PRG,*.ppx_PRW,*.ppx_PRX,*.ppx_PRG `
+    | Remove-Item -Force -ErrorAction SilentlyContinue
+
+}
+
+ExecutarCompilacao
